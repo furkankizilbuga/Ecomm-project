071c0f2eed35b4bb3d31b992eea212cdc762b371
furkankizilbuga <furkankizilbuga12@gmail.com>
2024-08-15 18:28:28 +0300

    desktop footer done

--- jun/src/layout/Footer.jsx
+++ jun/src/layout/Footer.jsx
@@ -1,38 +1,38 @@
 export default function Footer() {
     //TODO <Link> eklenecek.
     return(
-        <footer className="flex flex-col font-montserrat">
-            <div className="mt-20 mb-12 flex flex-col gap-5 mx-12">
-                <div className="flex flex-col gap-3 text-secondary-text text-sm">
-                    <h3 className="text-text-color text-xl font-bold">Get In Touch</h3>
+        <footer className="flex flex-col font-montserrat sm:border-2 sm:items-center">
+            <div className="mt-20 mb-12 flex flex-col gap-5 mx-12 sm:flex-row sm:justify-center sm:gap-16">
+                <div className="flex flex-col gap-3 text-secondary-text text-xs sm:max-w-44">
+                    <h3 className="text-text-color text-xl font-bold sm:text-lg">Get In Touch</h3>
                     <p>the quick fox jumps over the lazy dog</p>
-                    <div className="text-primary text-2xl flex gap-6">
+                    <div className="text-primary text-2xl flex gap-6 sm:text-lg sm:gap-4">
                         <i className="fa-brands fa-facebook"></i>
                         <i className="fa-brands fa-instagram"></i>
                         <i className="fa-brands fa-twitter"></i>
                     </div>
                 </div>
-                <div className="flex flex-col gap-3 text-secondary-text font-semibold text-sm">
-                    <h3 className="text-text-color text-xl font-bold">Company Info</h3>
-                    <nav className="flex flex-col gap-1">
+                <div className="flex flex-col gap-3 text-secondary-text font-semibold text-xs sm:max-w-44">
+                    <h3 className="text-text-color text-xl font-bold sm:text-lg">Company Info</h3>
+                    <nav className="flex flex-col gap-1 sm:gap-2">
                         <p className="">About us</p>
                         <p className="">Career</p>
                         <p className="">We are hiring</p>
                         <p className="">Blog</p>        
                     </nav>
                 </div>
-                <div className="flex flex-col gap-3 text-secondary-text font-semibold text-sm">
-                    <h3 className="text-text-color text-xl font-bold">Features</h3>
-                    <nav className="flex flex-col gap-1">
+                <div className="flex flex-col gap-3 text-secondary-text font-semibold text-xs sm:max-w-44">
+                    <h3 className="text-text-color text-xl font-bold sm:text-lg">Features</h3>
+                    <nav className="flex flex-col gap-1 sm:gap-2">
                         <p className="">Business Marketing</p>
                         <p className="">User Analytic</p>
                         <p className="">Live Chat</p>
                         <p className="">Unlimited Support</p>        
                     </nav>
                 </div>
-                <div className="flex flex-col gap-3 text-secondary-text font-semibold text-sm">
-                    <h3 className="text-text-color text-xl font-bold">Resources</h3>
-                    <nav className="flex flex-col gap-1"> 
+                <div className="flex flex-col gap-3 text-secondary-text font-semibold text-xs sm:max-w-44">
+                    <h3 className="text-text-color text-xl font-bold sm:text-lg">Resources</h3>
+                    <nav className="flex flex-col gap-1 sm:gap-2"> 
                         <p>IOS & Android</p>
                         <p>Watch a Demo</p>
                         <p>Customers</p>
@@ -40,7 +40,7 @@ export default function Footer() {
                     </nav>
                 </div>
             </div>
-            <div className="bg-[#FAFAFA] w-screen py-6">
+            <div className="bg-[#FAFAFA] w-full py-6">
                 <p className="text-secondary-text text-xs font-bold text-center mx-16">Made With Love By Figmaland All Right Reserved</p>
             </div>
         </footer>
--- jun/src/layout/Header.jsx
+++ jun/src/layout/Header.jsx
@@ -6,21 +6,21 @@ export default function Header() {
 
     return (
         
-        <header className="flex flex-col items-center mx-12 my-4 bg-white font-montserrat sm:flex-row sm:justify-between sm:px-9 sm:min-w-max sm:gap-4">
-            <div className="flex flex-col w-full sm:flex-row sm:items-center sm:w-auto sm:gap-14">
+        <header className="flex flex-col items-center mx-12 my-4 bg-white font-montserrat sm:flex-row sm:justify-between sm:min-w-max sm:gap-4">
+            <div className="flex flex-col w-full sm:flex-row sm:items-center sm:w-auto sm:gap-14 sm:max-[800px]:gap-5">
                 <div className="flex justify-between w-full sm:w-auto">
                     <h1 className="text-text-color font-bold text-xl sm:text-2xl">Jun</h1>
                     <button className="sm:hidden" onClick={() => setDisplay(!display)}><i className="fa-solid fa-list text-secondary-text"></i></button>
                 </div>
                 <nav className={`${display ? "opacity-1 mt-24 mb-16 gap-8" : "opacity-0 h-0 overflow-hidden"} flex flex-col duration-500 text-2xl transition ease-in-out sm:opacity-100 sm:h-auto sm:overflow-visible sm:flex-row sm:gap-4 sm:m-0 sm:mt-0 sm:mb-0`}>                    
-                    <button className="text-primary sm:text-base">Explore</button>
-                    <button className="text-secondary-text sm:text-base">Login</button>
-                    <button className="text-secondary-text sm:text-base text-nowrap">Sign-up</button>
+                    <button className="text-primary sm:text-base sm:max-[800px]:text-sm">Explore</button>
+                    <button className="text-secondary-text sm:text-base sm:max-[800px]:text-sm">Login</button>
+                    <button className="text-secondary-text sm:text-base text-nowrap sm:max-[800px]:text-sm">Sign-up</button>
                 </nav>
             </div>
             <div className="flex flex-col w-full gap-2 pt-10 sm:flex-row sm:gap-0 sm:pt-0 sm:w-auto">
-                <input placeholder="Search" className="bg-[#F9F9F9] focus:border-primary transition-all outline-none rounded border border-[#E6E6E6] px-3 py-1 placeholder:text-sm sm:rounded-l sm:rounded-r-none"/>
-                <select className="bg-[#F9F9F9] focus:border-primary transition-all outline-none rounded border border-[#E6E6E6] px-2 py-1 text-sm sm:rounded-none">
+                <input placeholder="Search" className="bg-[#F9F9F9] focus:border-primary transition-all outline-none rounded border border-[#E6E6E6] px-3 py-1 placeholder:text-sm sm:rounded-l sm:px-2 sm:rounded-r-none sm:max-[700px]:w-[150px]"/>
+                <select className="bg-[#F9F9F9] focus:border-primary transition-all outline-none rounded border border-[#E6E6E6] px-2 py-1 text-sm sm:rounded-none sm:px-0 sm:max-[800px]:gap-5">
                     <option disabled selected hidden>Category</option>
                     <option>Clothing</option>
                 </select>
