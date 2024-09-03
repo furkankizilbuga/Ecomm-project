49755eac6d133e8d2513c9ef07781c0337900c9f
furkankizilbuga <furkankizilbuga12@gmail.com>
2024-09-03 20:23:29 +0300

    Update HomePage.jsx

--- jun/src/pages/HomePage.jsx
+++ jun/src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-/* eslint-disable react/no-unescaped-entities */
+ 
 import BestSellerProducts from "@/layout/HomePageLayouts/BestSellerProducts";
 import EditorsPick from "@/layout/HomePageLayouts/EditorsPick";
 import FeaturedProducts from "@/layout/HomePageLayouts/FeaturedProducts";
