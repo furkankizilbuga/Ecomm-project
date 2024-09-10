393db9b43d279ec7ad4372c73a271616b10b8ae5
furkankizilbuga <furkankizilbuga12@gmail.com>
2024-09-10 22:11:21 +0300

    Update FeaturedProducts.jsx

--- jun/src/layout/HomePageLayouts/FeaturedProducts.jsx
+++ jun/src/layout/HomePageLayouts/FeaturedProducts.jsx
@@ -51,18 +51,18 @@ const sliderImages = {
 
 export default function FeaturedProducts() {
     return(
-        <div className="flex flex-col items-center gap-16 mt-16">
+        <div className="flex flex-col items-center gap-16 mt-16 sm:mt-16">
             <div className="flex">
                 <CarouselComp className="" sliderImages={sliderImages} />
             </div>
-            <div className="flex flex-col items-center justify-center  gap-6 sm:flex-row-reverse sm:gap-24">
-                <div className="flex flex-col items-center gap-6 sm:items-start">
-                    <p className="text-mutedColor font-bold text-sm">SUMMER 2020</p>
-                    <div className="text-center flex flex-col items-center gap-4 sm:text-left">
+            <div className="flex flex-col items-center justify-center gap-6 md:flex-row-reverse sm:gap-24">
+                <div className="flex flex-col items-center gap-6 md:items-start">
+                    <p className="text-mutedColor font-bold text-sm md:text-left">SUMMER 2020</p>
+                    <div className="text-center flex flex-col items-center gap-4 md:text-left">
                         <h2 className="text-textColor max-w-56 font-bold text-4xl sm:max-w-96">Part of the Neural Universe</h2>
-                        <p className="max-w-52 text-center text-secondaryTextColor font-semibold sm:text-left sm:max-w-96">We know how large objects will act, but things on a small scale.</p>
+                        <p className="max-w-52 text-center text-secondaryTextColor font-semibold md:text-left sm:max-w-96">We know how large objects will act, but things on a small scale.</p>
                     </div>
-                    <div className="flex-col flex gap-4 mb-6 sm:flex-row">
+                    <div className="flex-col flex gap-4 mb-6 sm:flex-row sm:justify-center">
                         <button className="text-white bg-primaryBlue font-bold rounded text-xs py-3 px-8">BUY NOW</button>
                         <button className="text-primaryBlue rounded text-xs font-bold border border-primaryBlue py-3 px-8">Learn More</button>
                     </div>
