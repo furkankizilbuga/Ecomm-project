d2350ac9abab2eb7c88b99c7b6b2e95f699ae863
furkankizilbuga <furkankizilbuga12@gmail.com>
2024-09-10 20:11:07 +0300

    update

--- jun/src/layout/HomePageLayouts/BestSellerProducts.jsx
+++ jun/src/layout/HomePageLayouts/BestSellerProducts.jsx
@@ -11,13 +11,13 @@ const productCardImages = [
 
 export default function BestSellerProducts() {
     return (
-        <div className="flex flex-col gap-10 px-4 sm:pt-16">
+        <div className="flex flex-col items-center gap-10 px-4 sm:pt-16">
             <div className="flex flex-col items-center gap-3">
                 <p className="max-w-52 text-center text-secondaryTextColor text-xl font-semibold">Featured Products</p>
                 <h3 className="max-w-52 text-textColor font-bold text-2xl text-center sm:max-w-96">BEST SELLER PRODUCTS</h3>
                 <p className="max-w-52 text-center text-secondaryTextColor sm:max-w-96">Problems trying to resolve the conflict between</p>
             </div>
-            <div className="grid gap-x-8 gap-y-12 justify-center max-w-screen-xl mx-auto 2xl:grid-cols-4 xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-1">
+            <div className="flex flex-col items-center gap-x-8 gap-y-12 justify-center sm:flex-wrap sm:flex-row sm:max-w-8xl">
                 {
                     productCardImages.map((item, index) => 
                         <ProductCard key={index} item={item} />
