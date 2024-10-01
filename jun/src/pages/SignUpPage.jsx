2c7c61c39fb48ceb0b5cd2ef9789ce21df87caec
furkankizilbuga <furkankizilbuga12@gmail.com>
2024-10-01 15:21:21 +0300

    Update SignUpPage.jsx

--- jun/src/pages/SignUpPage.jsx
+++ jun/src/pages/SignUpPage.jsx
@@ -1,18 +1,26 @@
-import { useForm } from "react-hook-form"
+import { useForm, useWatch } from "react-hook-form"
 
 export default function SignUpPage() {
 
     const {
         register,
         handleSubmit,
-        formState: { errors, isValid},
+        formState: { errors, isValid },
+        control
     } = useForm({
         defaultValues: {
             name: "",
             email: "",
-            password: ""
+            password: "",
+            role_id: 1,
+
         },
         mode: "onSubmit"
+    })
+
+    const roleId = useWatch({
+        control,
+        name: "role_id"
     })
 
     const onSubmit = () => {
@@ -22,16 +30,18 @@ export default function SignUpPage() {
     return(
         <div className="py-20 mx-10">
             <form className="flex flex-col items-center gap-4" onSubmit={handleSubmit(onSubmit)}>
-                <div className="w-full">
+                <div className="w-full flex flex-col gap-2">
+                    <label className="text-textColor font-bold">Name *</label>
                     <input
-                        className="bg-[#F9F9F9] w-full placeholder:text-sm focus:border-primaryBlue transition-all outline-none rounded border border-[#E6E6E6] py-2 pl-2"
+                        className="bg-[#F9F9F9] w-full placeholder:text-sm focus:border-primaryBlue transition-all outline-none rounded border border-[#E6E6E6] py-3 pl-4"
                         placeholder="Name"
                         {...register("name", { required: "Lütfen isminizi giriniz!", minLength: 3})}/> 
                     {errors.name && <span>{errors.name.message}</span>}
                 </div>
-                <div className="w-full">
+                <div className="w-full flex flex-col gap-2">
+                    <label className="text-textColor font-bold">Email *</label>
                     <input 
-                        className="bg-[#F9F9F9] w-full placeholder:text-sm focus:border-primaryBlue transition-all outline-none rounded border border-[#E6E6E6] py-2 pl-2"
+                        className="bg-[#F9F9F9] w-full placeholder:text-sm focus:border-primaryBlue transition-all outline-none rounded border border-[#E6E6E6] py-3 pl-4"
                         placeholder="Email"
                         {...register("email", { 
                             required: "Lütfen mailinizi giriniz!", 
@@ -43,9 +53,10 @@ export default function SignUpPage() {
                         })}/>
                     {errors.email && <span>{errors.email.message}</span>}
                 </div>
-                <div className="w-full">
+                <div className="w-full flex flex-col gap-2">
+                    <label className="text-textColor font-bold">Password *</label>
                     <input
-                        className="bg-[#F9F9F9] w-full placeholder:text-sm focus:border-primaryBlue transition-all outline-none rounded border border-[#E6E6E6] py-2 pl-2"
+                        className="bg-[#F9F9F9] w-full placeholder:text-sm focus:border-primaryBlue transition-all outline-none rounded border border-[#E6E6E6] py-3 pl-4"
                         placeholder="Password"
                         {...register("password", { 
                             required: "Lütfen şifrenizi giriniz!", 
@@ -56,9 +67,10 @@ export default function SignUpPage() {
                             })}/>  
                     {errors.password && <span>{errors.password.message}</span>}
                 </div>
-                <div className="w-full">
+                <div className="w-full flex flex-col gap-2">
+                    <label className="text-textColor font-bold">Password Validation *</label>
                     <input
-                        className="bg-[#F9F9F9] w-full placeholder:text-sm focus:border-primaryBlue transition-all outline-none rounded border border-[#E6E6E6] py-2 pl-2"
+                        className="bg-[#F9F9F9] w-full placeholder:text-sm focus:border-primaryBlue transition-all outline-none rounded border border-[#E6E6E6] py-3 pl-4"
                         placeholder="Password Validation"
                         {...register("password", { 
                             required: "Lütfen şifrenizi tekrar giriniz!", 
@@ -69,6 +81,35 @@ export default function SignUpPage() {
                             })}/>  
                     {errors.password && <span>{errors.password.message}</span>}
                 </div>
+                <div className="w-full flex flex-col gap-2">
+                    <label className="text-textColor font-bold">Role *</label>
+                    <select 
+                        {...register("role_id")}
+                        className="bg-[#F9F9F9] w-full focus:border-primaryBlue transition-all font-semibold text-textColor outline-none rounded border border-[#E6E6E6] py-3 pl-4 text-sm">
+                        <option value="3" className="font-medium text-textColor" selected>Customer</option>
+                        <option value="1" className="font-medium text-textColor">Admin</option>
+                        <option value="2" className="font-medium text-textColor">Store</option>
+                    </select>
+                </div>
+                {
+                    roleId == 2 && 
+                    <>
+                        <div className="w-full flex flex-col gap-2">
+                            <label className="text-textColor font-bold">Password *</label>
+                            <input
+                                className="bg-[#F9F9F9] w-full placeholder:text-sm focus:border-primaryBlue transition-all outline-none rounded border border-[#E6E6E6] py-3 pl-4"
+                                placeholder="Store Name"
+                                {...register("password", { 
+                                    required: "Lütfen şifrenizi giriniz!", 
+                                    pattern: {
+                                        value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/,
+                                        message: "Şifreniz en az: bir büyük, bir küçük harf, bir özel karakter ve bir sayı içermelidir!"
+                                    }
+                                    })}/>  
+                            {errors.password && <span>{errors.password.message}</span>}
+                        </div>
+                    </>
+                }
                 <button 
                     type="submit"
                     disabled={ !isValid }
