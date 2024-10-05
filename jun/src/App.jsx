efb853312ac046c98ba5a53a0e58a78d8aa222f0
furkankizilbuga <furkankizilbuga12@gmail.com>
2024-10-05 12:28:33 +0300

    Update App.jsx

--- jun/src/App.jsx
+++ jun/src/App.jsx
@@ -1,17 +1,61 @@
+import { useEffect } from 'react'
 import './App.css'
 import Footer from './layout/Footer'
 import Header from './layout/Header'
 import PageContent from './layout/PageContent'
-import { useLocation } from 'react-router-dom'
-import { ToastContainer } from "react-toastify";
+import { useLocation, useHistory } from 'react-router-dom'
+import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import axios from 'axios'
+import { useDispatch } from 'react-redux'
+import { setUser } from './store/features/clientSlice'
 
 
 function App() {
   
   const location = useLocation();
+  const dispatch = useDispatch();
+  const history = useHistory();
 
   const show = location.pathname !== "/contact" && location.pathname !== "/team";
+
+
+  //Auto Login Control
+  useEffect(() => {
+
+    const token = localStorage.getItem("token");
+
+    if(token != null) {
+
+      const baseURL = "https://workintech-fe-ecommerce.onrender.com";
+
+      axios.get(baseURL + "/verify",{
+        headers: {
+          Authorization: token
+        }
+      }
+      )
+      .then(res => {
+
+        const user = res.data;
+        dispatch(setUser({name: user.name, email: user.email}))
+        localStorage.setItem("token", user.token);
+
+      })
+      .catch(err => {
+        if (err.response && err.response.status === 401) {
+          toast.error("Lütfen tekrar giriş yapınız.");
+          localStorage.removeItem("token");
+        } else {
+          console.log(err);
+        }
+
+        history.push("/");
+      })
+
+    }
+
+  }, [dispatch, history])
 
   return (
     <>
