3300ff850edf37af0b341cd04f5c92fda4973872
furkankizilbuga <furkankizilbuga12@gmail.com>
2024-10-08 16:14:51 +0300

    Update ShopProducts.jsx

--- jun/src/layout/ShopPageLayouts/ShopProducts.jsx
+++ jun/src/layout/ShopPageLayouts/ShopProducts.jsx
@@ -2,6 +2,7 @@ import ProductCard from "@/components/ProductCard"
 import Spinner from "@/components/Spinner";
 import useImageSize from "@/hooks/useImageSize"
 import { fetchStates } from "@/store/features/productSlice";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
 export default function ShopProducts() {
@@ -16,25 +17,73 @@ export default function ShopProducts() {
 
     const { products, productsFetchState, categories } = useSelector(state => state.product);
 
+    const [selectedCategory, setSelectedCategory] = useState("");
+    const [filterText, setFilterText] = useState("");
+    const [sortOption, setSortOption] = useState("");
+    const [productCopy, setProductCopy] = useState(products);
+
+    useEffect(() => {
+        setProductCopy(products);
+      }, [products]);
+
+    const filterHandler = () => {
+        
+        let temp = productCopy.filter(product => product.description.toLocaleLowerCase().includes(filterText.toLocaleLowerCase()));
+
+        switch (sortOption) {
+            case "priceAsc":
+                temp.sort((a, b) => a.price - b.price);
+                break;
+            case "priceDesc":
+                temp.sort((a, b) => b.price - a.price);
+                break;
+            case "rateAsc":
+                temp.sort((a, b) => a.rating - b.rating);
+                break;
+            case "rateDesc":
+                temp.sort((a, b) => b.rating - a.rating);
+                break;
+            default:
+                break;
+
+        }
+
+        setProductCopy(temp);
+    }
+
     return(
         <div className="flex flex-col items-center justify-center pt-10 gap-20">
             <div className="flex flex-col items-center gap-6 min-[920px]:flex-row sm:justify-between sm:w-full sm:px-40">
                 <p className="text-sm font-semibold text-secondaryTextColor">Showing all 12 results</p>
-                <div className="flex gap-4 items-center">
-                    <p className="text-sm font-semibold text-secondaryTextColor">Views: </p>
-                    <div className="flex gap-2">
-                        <button className="border border-[#E6E6E6] px-3 py-2 rounded text-sm"><i className="fa-solid fa-table-cells-large"></i></button>
-                        <button className="border border-[#E6E6E6] px-3 py-2 rounded text-sm"><i className="fa-solid fa-list"></i></button>
+                
+                <div className="flex flex-col items-center sm:items-end gap-4">
+                    <div className="flex flex-col sm:flex-row">
+                        <select 
+                            value={sortOption}
+                            onChange={e => setSortOption(e.target.value)}
+                            className="bg-[#F9F9F9] focus:border-primaryBlue transition-all outline-none border rounded-l border-[#E6E6E6] px-4 py-2 text-sm sm:px-4 sm:max-[800px]:gap-5">
+                            <option value="" selected hidden disabled>Sort</option>
+                            <option value="priceDesc">Price Desc</option>
+                            <option value="priceAsc">Price Asc</option>
+                            <option value="rateDesc">Rating Desc</option>
+                            <option value="rateAsc">Rating Asc</option>
+                        </select>   
+                        <input value={filterText} onChange={(e) => setFilterText(e.target.value)} placeholder="Filter"  className="bg-[#F9F9F9] focus:border-primaryBlue transition-all outline-none border border-[#E6E6E6] px-4 py-2 text-sm sm:px-4 sm:max-[800px]:gap-5"/>
+                        <select className="bg-[#F9F9F9] focus:border-primaryBlue transition-all outline-none border border-[#E6E6E6] px-4 py-2 text-sm sm:px-4 sm:max-[800px]:gap-5">
+                            <option disabled selected hidden>Category</option>
+                            {categories.map((item, index)=> (
+                            <option key={index}>{item.title}</option>
+                            ))}
+                        </select>
+                        <button onClick={filterHandler} className="bg-primaryBlue py-2 px-6 rounded-r text-sm text-white">Filter</button>
                     </div>
-                </div>
-                <div className="flex gap-2">
-                    <select className="bg-[#F9F9F9] focus:border-primaryBlue transition-all outline-none rounded border border-[#E6E6E6] px-4 py-2 text-sm sm:px-4 sm:max-[800px]:gap-5">
-                        <option disabled selected hidden>Category</option>
-                        {categories.map((item, index)=> (
-                        <option key={index}>{item.title}</option>
-                        ))}
-                    </select>
-                    <button className="bg-primaryBlue px-6 rounded text-sm text-white">Filter</button>
+                    <div className="flex gap-4 items-center">
+                        <p className="text-sm font-semibold text-secondaryTextColor">Views: </p>
+                        <div className="flex gap-2">
+                            <button className="border border-[#E6E6E6] px-3 py-2 rounded text-sm"><i className="fa-solid fa-table-cells-large"></i></button>
+                            <button className="border border-[#E6E6E6] px-3 py-2 rounded text-sm"><i className="fa-solid fa-list"></i></button>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className="flex flex-col items-center gap-x-8 gap-y-12 justify-center sm:flex-wrap sm:flex-row sm:px-40 sm:max-w-8xl">
@@ -42,7 +91,7 @@ export default function ShopProducts() {
                     productsFetchState === fetchStates.FETCHING ? (
                         <Spinner />
                     ) : (
-                        products.map(item => 
+                        productCopy.map(item => 
                             <ProductCard key={item.id} item={item} />
                         )
                     )
