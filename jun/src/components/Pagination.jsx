5662292664230cb54e0fbbd372617644a2c8537c
furkankizilbuga <furkankizilbuga12@gmail.com>
2024-10-09 16:16:20 +0300

    Update Pagination.jsx

--- jun/src/components/Pagination.jsx
+++ jun/src/components/Pagination.jsx
@@ -7,20 +7,50 @@ export default function Pagination({ productsPerPage, totalProducts, paginate, c
         pageNumbers.push(i);
     }
 
+    const handlePrevious = () => {
+        if (currentPage > 1) {
+            paginate(currentPage - 1);
+        }
+    };
+
+    const handleNext = () => {
+        if (currentPage < pageNumbers.length) {
+            paginate(currentPage + 1);
+        }
+    };
+
     return (
         <nav>
-            <ul className="flex flex-row">
+            <ul className="flex flex-row items-center">
+                <li className="bg-primaryBlue text-white font-semibold rounded-l-md">
+                    <button 
+                        onClick={handlePrevious} 
+                        disabled={currentPage === 1} 
+                        className="border px-4 py-2 text-sm h-10 flex items-center justify-center disabled:opacity-50"
+                    >
+                        Prev
+                    </button>
+                </li>
                 {pageNumbers.map(number => (
                     <li key={number}>
                         <Link 
                             to={`?page=${number}`}
                             onClick={() => paginate(number)}
-                            className={`border font-semibold text-primaryBlue px-4 py-2 ${number === 1 ? 'rounded-l' : ''} ${number == currentPage && `bg-primaryBlue text-white`} ${number === pageNumbers.length ? 'rounded-r' : ''} block w-full text-center`}
+                            className={`border text-primaryBlue font-semibold px-4 py-2 h-10 flex items-center justify-center ${number === currentPage ? 'bg-primaryBlue text-white' : ''} block w-full text-center`}
                         >
                             {number}
                         </Link>
                     </li>
                 ))}
+                <li className='bg-primaryBlue text-white font-semibold rounded-r-md'>
+                    <button 
+                        onClick={handleNext} 
+                        disabled={currentPage === pageNumbers.length} 
+                        className="border px-4 py-2 text-sm h-10 flex items-center justify-center disabled:opacity-50"
+                    >
+                        Next
+                    </button>
+                </li>
             </ul>
         </nav>
     );
