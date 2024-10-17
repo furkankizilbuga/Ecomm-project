f54aa1a4fe073400077767ff20dff53e3483f5b3
furkankizilbuga <furkankizilbuga12@gmail.com>
2024-10-17 21:08:53 +0300

    Update Cart.jsx

--- jun/src/components/Cart.jsx
+++ jun/src/components/Cart.jsx
@@ -13,21 +13,20 @@ const Cart = () => {
                 onClick={() => setCartDisplay(!cartDisplay)}
                 className="fa-solid fa-cart-shopping relative rounded-full w-16 h-16 pt-4 text-2xl sm:pt-2 sm:w-10 sm:h-10 text-center sm:text-base text-primaryBlue my-auto bg-white border border-primaryBlue">
             </i>
-            <div className={`${cartDisplay || "hidden"} bg-red-200 px-4 py-2 flex flex-col absolute w-52 border border-primaryBlue rounded-md`}>
-                <h2 className="text-primaryBlue text-base" >My Cart {cart.length}</h2>
+            <div className={`${cartDisplay || "hidden"} bg-white p-4 flex flex-col absolute w-52 border border-primaryBlue rounded-md`}>
+                <h2 className="text-primaryBlue font-semibold text-base" >My Cart {cart.length}</h2>
                 {cart.map(({product, count, checked}, index) => 
-                    <div key={index} className="flex gap-2 text-sm border-b border-b-primaryBlue py-4">
-                        <div className="flex items-center border w-24 max-w-10 border-mutedColor rounded">
-                            <img src={product.images[0].url} alt="product-image" className="object-cover object-center h-16 rounded" />
+                    <div key={index} className="flex gap-2 h-32 w-full text-sm border-b border-b-primaryBlue py-4">
+                        <div className="flex items-center border border-mutedColor rounded min-w-12">
+                            <img src={product.images[0].url} alt="product-image" className="w-12 object-cover object-center h-16 rounded" />
                         </div>
-                        <div className="flex flex-col gap-1">
-                            <h3 className="text-textColor">{product.name}</h3>
+                        <div className="flex flex-col justify-between gap-1">
+                            <h3 className="text-textColor text-xs font-medium">{product.name}</h3>
                             <p className="text-[8px] text-secondaryTextColor leading-3">{product.description}</p>
                             <div className="flex justify-between w-20">
-                                <span className="text-primaryBlue">{product.price}</span>
+                                <span className="text-primaryBlue font-medium">${product.price}</span>
                                 <span>{count}</span>
                             </div>
-        
                         </div>
                     </div>
                 )}
