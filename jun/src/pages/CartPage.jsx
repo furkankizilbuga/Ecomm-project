b7fb98ebfabe2f4776d5fc514295424908200bb0
furkankizilbuga <furkankizilbuga12@gmail.com>
2024-10-20 20:46:02 +0300

    Update CartPage.jsx

--- jun/src/pages/CartPage.jsx
+++ jun/src/pages/CartPage.jsx
@@ -6,10 +6,10 @@ const CartPage = () => {
 
     return (
         <div className="flex flex-col mx-12">
-            <h2>My Cart (2 Ürün)</h2>
-            <div className="flex flex-col">
+            <h2 className="text-textColor font-semibold">My Cart ({cart.length})</h2>
+            <div className="flex flex-col gap-4 sm:border border-mutedColor rounded py-4">
                 {cart.map(({product, count, checked}, index) => 
-                    <div className="flex items-center gap-2" key={index}>
+                    <div className="flex flex-col items-center gap-2 border border-mutedColor rounded px-2 py-4" key={index}>
                         <input type="checkbox" />
                         <div className="flex gap-2">
                             <div className="flex items-center border border-mutedColor rounded min-w-20 max-w-20 h-40 sm:min-w-24">
@@ -17,22 +17,22 @@ const CartPage = () => {
                             </div>
                             <div>
                                 <h3 className="font-semibold text-textColor">{product.name}</h3>
-                                <p className="text-secondaryTextColor text-sm">{product.description}</p>
+                                <p className="text-secondaryTextColor text-xs overflow-ellipsis">{product.description}</p>
                             </div>
                         </div>
-                        <div className="flex gap-2">
-                            <div className="flex">
-                                <button>-</button>
-                                <span>{count}</span>
-                                <button>+</button>
+                        <div className="flex justify-between items-center h-full w-full">
+                            <div className="flex items-center w-20 h-full">
+                                <button className="border rounded-l h-8 border-mutedColor px-2 font-semibold text-primaryBlue">-</button>
+                                <span className="border-t border-b h-8 flex w-full justify-center items-center border-mutedColor px-2 font-medium text-textColor text-sm">{count}</span>
+                                <button className="border rounded-r h-8 border-mutedColor px-2 font-semibold text-primaryBlue">+</button>
                             </div>
-                            <span>{product.price}</span>
-                            <i className="fa-solid fa-trash"></i>
+                            <span className="font-semibold text-sm">${product.price}</span>
+                            <i className="fa-solid fa-trash text-primaryBlue"></i>
                         </div>
                     </div>
                 )}
-                
             </div>
+            <button className="bg-primaryBlue text-white text-xs font-semibold w-full py-2 rounded">Purchase</button>
         </div>
     )
 }
