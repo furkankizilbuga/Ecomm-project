6fbfd9faf680eb5b4abfba6801c8e082a08d562f
furkankizilbuga <furkankizilbuga12@gmail.com>
2024-10-23 16:34:52 +0300

    Update CreateNewAddress.jsx

--- jun/src/components/CreateOrderPageComponents/CreateNewAddress.jsx
+++ jun/src/components/CreateOrderPageComponents/CreateNewAddress.jsx
@@ -1,6 +1,15 @@
+import { useAuth } from "@/hooks/useAuth";
+import { fetchAddressList } from "@/store/features/clientSlice";
+import axios from "axios";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
 
 const CreateNewAddress = () => {
+
+    const dispatch = useDispatch();
+    const { token } = useAuth();
+    const [errorMessage, setErrorMessage] = useState("");
 
     const {
         register,
@@ -16,42 +25,89 @@ const CreateNewAddress = () => {
             district: "",
             neighborhood: ""
         },
-        mode: "onChange"
+        mode: "onSubmit"
     });
 
+    const onSubmit = (data) => {
+
+        const baseURL = "https://workintech-fe-ecommerce.onrender.com"
+
+        axios.post(baseURL + "/user/address", data)
+        .then(() => {
+            dispatch(fetchAddressList(token))
+            setErrorMessage("");
+        })
+        .catch(err => {
+            setErrorMessage("Please try again.");
+            console.log(err)
+        })
+
+    }
+
     return(
-        <form className="flex flex-col gap-2">
-            <input
-                placeholder="Title" 
-                className="border pl-1 rounded w-full outline-1 outline-primaryBlue placeholder:text-xs" />
-            <div className="shadow p-4">
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-2">
+            {errorMessage && <span className="text-xs font-medium text-red-500">{errorMessage}</span>}
+            {Object.keys(errors).length > 0 && <span className="text-xs font-medium text-red-500">Please fill all form fields!</span>}
+            {errors.phone && <span className="text-xs font-medium text-red-500">Please use a valid phone!</span>}
+                <input
+                    {...register("title", {
+                        required: "Title is required!"
+                    })}
+                    placeholder="Title" 
+                    className="border pl-1 py-1 rounded w-full outline-1 outline-primaryBlue placeholder:text-xs" />
+            <div className="shadow p-4 rounded">
                 <div className="flex flex-col gap-1">
                     <div className="flex gap-2 items-center">
-                        <i className="fa-solid fa-user text-sm"></i>
+                        <div className="flex items-center gap-2">
+                            <i className="fa-solid fa-user text-sm"></i>
+                            <input
+                                {...register("name", {
+                                    required: "Name is required!"
+                                })}
+                                placeholder="Name" 
+                                className="pl-1 py-1 border w-full outline-1 outline-primaryBlue rounded text-sm placeholder:text-xs"/>
+                        </div>
                         <input
-                            placeholder="Name" 
-                            className="pl-1 border w-full outline-1 outline-primaryBlue rounded text-sm placeholder:text-xs"/>
-                        <input
+                            {...register("surname", {
+                                required: "Surname is required!"
+                            })}
                             placeholder="Surname" 
-                            className="pl-1 border w-full outline-1 outline-primaryBlue rounded text-sm placeholder:text-xs"/>
+                            className="pl-1 py-1 border w-full outline-1 outline-primaryBlue rounded text-sm placeholder:text-xs"/>
                     </div>
                     <div className="flex gap-2 items-center">
                         <i className="fa-solid fa-mobile-alt text-sm"></i>    
                         <input
+                            {...register("phone", {
+                                required: "Phone is required!",
+                                pattern: {
+                                    value: /^(?:\+90[\s-]?)?0?5\d{2}[\s-]?\d{3}[\s-]?\d{2}[\s-]?\d{2}$/,
+                                    message: "Please enter a valid number!"
+                                }
+                            })}
                             placeholder="Phone" 
-                            className="pl-1 border w-full outline-1 outline-primaryBlue rounded text-sm placeholder:text-xs"/>
+                            className="pl-1 py-1 border w-full outline-1 outline-primaryBlue rounded text-sm placeholder:text-xs"/>
                     </div>
                     <input 
+                        {...register("city", {
+                            required: "City is required!"
+                        })}
                         placeholder="City"
-                        className="pl-1 border w-full outline-1 outline-primaryBlue rounded text-sm placeholder:text-xs" />
+                        className="pl-1 py-1 border w-full outline-1 outline-primaryBlue rounded text-sm placeholder:text-xs" />
                     <input 
+                        {...register("district", {
+                            required: "District is required!"
+                        })}
                         placeholder="District"
-                        className="pl-1 border w-full outline-1 outline-primaryBlue rounded text-sm placeholder:text-xs" />
+                        className="pl-1 py-1 border w-full outline-1 outline-primaryBlue rounded text-sm placeholder:text-xs" />
                     <input 
+                        {...register("neighborhood", {
+                            required: "Neighborhood is required!"
+                        })}
                         placeholder="Neighborhood"
-                        className="pl-1 border w-full outline-1 outline-primaryBlue rounded text-sm placeholder:text-xs" />         
+                        className="pl-1 py-1 border w-full outline-1 outline-primaryBlue rounded text-sm placeholder:text-xs" />         
                 </div>
             </div>
+            <button type="submit" className="font-medium text-white text-sm bg-primaryBlue rounded py-1">Save</button>
         </form>
     )
 }
