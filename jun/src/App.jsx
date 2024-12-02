4a28b9ceb8bee1dee1d315dfe33effc47a2a97c5
furkankizilbuga <furkankizilbuga12@gmail.com>
2024-12-03 00:17:03 +0300

    Update App.jsx

--- jun/src/App.jsx
+++ jun/src/App.jsx
@@ -6,11 +6,21 @@ import { useLocation } from 'react-router-dom'
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { AuthProvider } from './store/AuthContext'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { fetchCategories } from './store/features/productSlice'
 
 function App() {
   
   const location = useLocation();
   const show = location.pathname !== "/contact" && location.pathname !== "/team";
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCategories());
+  }, [dispatch])
+
 
   return (
     <AuthProvider>
